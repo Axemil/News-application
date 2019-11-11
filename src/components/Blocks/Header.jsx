e5cba1519d2d1a09eae302d98214b53bd1008ce8
Axemil <46081813+Axemil@users.noreply.github.com>
2019-11-12 01:50:56 +0200

    done header almost

--- src/components/Blocks/Header.jsx
+++ src/components/Blocks/Header.jsx
@@ -1,31 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faNewspaper,faAnchor, faFlag, faAddressCard, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import WOW from 'wow.js';
 
 const Header = () => {
+  useEffect(() => {
+    const wow = new WOW();
+    wow.init();
+  })
   return (
     <div className="header">
       <div className="div__header">
-        <div className="logo">
+        <div className="logo wow fadeInLeft">
           <Link className="link-hover" to="/"><FontAwesomeIcon icon={faNewspaper} /> News</Link>
         </div>
         <nav>
           <ul>
             <li>
-              <Link className="link-hover" to="/Hot"><FontAwesomeIcon icon={faAnchor} /> Новости Одессы</Link>
+              <Link className="link-hover wow fadeIn" to="/Hot"><FontAwesomeIcon icon={faAnchor} /> Новости Одессы</Link>
             </li>
             <li>
-              <Link className="link-hover" to="/UK"><FontAwesomeIcon icon={faFlag} /> Новости Украины</Link>
+              <Link className="link-hover wow fadeIn" to="/UK"><FontAwesomeIcon icon={faFlag} /> Новости Украины</Link>
             </li>
             <li>
-              <Link className="link-hover" to="/US"><FontAwesomeIcon icon={faAddressCard} /> О проекте</Link>
+              <Link className="link-hover wow fadeIn" to="/US"><FontAwesomeIcon icon={faAddressCard} /> О проекте</Link>
             </li>
             <li>
-              <Link className="link-hover" to="/search"><FontAwesomeIcon icon={faSearch} /> Поиск</Link>
+              <Link className="link-hover wow fadeIn" to="/search"><FontAwesomeIcon icon={faSearch} /> Поиск</Link>
             </li>
           </ul>
         </nav>
+        
       </div>
     </div>
   );
