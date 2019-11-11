47a2abd6b6417bbeddd53915d9401a4642ad5947
Axemil <46081813+Axemil@users.noreply.github.com>
2019-11-11 22:39:23 +0200

    fix commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/sass/App.scss';
+import '../assets/sass/normalize.scss';
+const App = () => {
+  return (
+    <div className='wrapper'>
+      <h1>Hello React</h1>
+      
+    </div>
+  )
+}
+
+export default App
