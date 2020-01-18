ac63dc0900f64269a3cd5a652ae6cb21250f7f09
Axemil <46081813+Axemil@users.noreply.github.com>
2020-01-18 16:13:15 +0200

    router fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("app")
 );
